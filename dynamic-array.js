231425bc2569ce1c43f45389faa63b46594e4a0c
Sami <samisamman26@yahoo.com>
2023-10-03 15:54:02 -0700

    finished

--- dynamic-array.js
+++ dynamic-array.js
@@ -10,13 +10,15 @@ class DynamicArray {
   }
 
   push(val) {
+    if (this.length === this.capacity) {
+      this.resize()
+    }
     this.data[this.length] = val;
     this.length++;
   }
 
   pop() {
     let popped = this.data[this.length - 1];
-
     this.data[this.length - 1] = undefined;
     if (this.length > 0) {
       this.length--;
@@ -38,6 +40,9 @@ class DynamicArray {
   }
 
   unshift(val) {
+    if (this.length === this.capacity) {
+      this.resize()
+    }
     for (let i = this.length; i > 0; i--) {
       this.data[i] = this.data[i - 1];
     }
@@ -48,11 +53,26 @@ class DynamicArray {
   }
 
   indexOf(val) {
-    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) return i
+    }
+    return -1
   }
 
   resize() {
-    // Your code here
+    // Resize makes a new, larger array then copies all the elements
+    // from the original this.data array into the new array. Then
+    // reassign the data to the new array.
+
+    let newArr = new DynamicArray(this.capacity * 2)
+    for (let i = 0; i<this.length; i++) {
+      newArr.data[i] = this.data[i]
+      newArr.length++
+    }
+    this.data = newArr.data
+    this.capacity = newArr.capacity
+    this.length = newArr.length
+
   }
 }
 
